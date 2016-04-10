dce23015e692564a9369e141947c70730e7c7810
treharne <R.Treharne@liverpool.ac.uk>
2016-04-10 19:24:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>dpmod.git</h1>
+<h2>Dielectric Permittivity Modelling</h2>
+
+Start: 09 April 2016
+Author: R. Treharne
---
+++ dpmod.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Material:
+    
+    def __init__(self):
+        self.config = []
+
+    def add(self, mod=Lorentz())
+        self.config.append(mod)
+
+class Lorentz:
+
+    def __init__(self, A=1, E=14):
+        return self.f(A, E)
+
+    def f(A, E):
+        return [A, E]
