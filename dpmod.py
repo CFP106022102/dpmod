1d4c2c8c42c7e2ef291401d8a0a9093277fe2329
treharne <R.Treharne@liverpool.ac.uk>
2016-04-13 14:33:39 +0100

    -m

--- dpmod.py
+++ dpmod.py
@@ -1,46 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 
 class Lorentz:
     
-    def __init__(self, A=22, E0=32, G = 1):
-        self.E = np.linspace(E0-5, E0+4, 100)
+    def __init__(self, E=np.linspace(5, 15, 100), A=10, E0=11, G = 1):
+        self.E = E
         self.A = A
         self.E0 = E0
         self.G = G
-
         self.param = {'Amp.': self.A,
-                       'E0 (eV)': self.E0,
-                       'Gamma (eV^-1)': self.G}
-        return None
+                      'E0': self.E0,
+                      'Damping': self.G}
 
-    def p(self):
-        p = self.param.values()
-        return p
+        return None
 
     def func(self):
         E = self.E
 
-        p = self.p()
-        A = p[0]
-        E0 = p[1]
-        G = p[2]
+        A = self.A
+        E0 = self.E0
+        G = self.G
 
         e1 = []
+        e2 = []
         
         for l in E:
-            e1.append(1 + ((E0**2-l**2)/((E0**2-l**2)**2+(G*l)**2)))
+            e1.append(1 + A**2*((E0**2-l**2)/((E0**2-l**2)**2+(G*l)**2)))
+            e2.append((A**2*G*l)/((E0**2-l**2)**2+(G*l)**2))
         
-        return e1
+        return np.array(e1) - np.array(e2)*1j
 
     def plot(self):
         E = self.E
 
-        l1, = plt.plot(E, self.func())
+        e = self.func()
+        l1, = plt.plot(E, np.real(e))
+        l2, = plt.plot(E, -np.imag(e))
         plt.show()
 
+    def table(self):
+        table = []
+        count = 1
+        param = self.param
+        for p in param:
+            table.append([count, p, param[p]])
+            count +=1
+
+        print tabulate(table,
+                       headers = ['#', 'Parameter', 'Value'],
+                       tablefmt = 'orgtbl')
+            
         
-       
 
 class Material:
     def __init__(self):
@@ -53,3 +64,21 @@ class Material:
 
     def dp(self, E=None):
         return None
+
+    def plot(self):
+        return None 
+
+    def table(self, item=None):
+        if item==None:
+            table = []
+            count = 1
+            for item in self.config:
+                table.append([count, item.__class__.__name__])
+                count += 1
+
+            print tabulate(table,
+                           headers = ['#', 'Name'],
+                           tablefmt = 'orgtbl')
+
+        else:
+            self.config[item-1].table()
