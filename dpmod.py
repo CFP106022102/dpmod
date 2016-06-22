d573da03f2c9e2b35f67a3acdbc3788437e13e55
Rob <R.Treharne@liverpool.ac.uk>
2016-06-22 17:06:24 +0100

    reinit

--- dpmod.py
+++ dpmod.py
@@ -1,45 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tabulate import tabulate
-
-h=6.63e-34
-e=1.602e-19
-c=3e8
-
-def unit(E, u='eV'):
-    if u=='eV':
-        return E
-    elif u=='nm':
-        x = (h*c)/(E*e*1e-9)
-        return x
-    elif u=='micron':
-        x = (h*c)/(E*e*1e-6)
-        return x
-
-def plot(obj, units='eV'):
-
-    '''Global plotting method'''
-
-    fig = plt.figure(figsize=(9,7))
-    ax1 = fig.add_subplot(111)
-    ax2 = ax1.twinx()
-
-    x = unit(obj.E, u=units)
-    e = obj.func(E=obj.E)
-
-    e1, = ax1.plot(x, np.real(e), color='red', label='$e_{1}$')
-    e2, = ax2.plot(x, np.imag(e), color='blue', label='$e_{2}$')
-
-    ax1.set_xlim(min(x), max(x))
-    ax1.set_ylim(min(np.real(e)), max(np.real(e)))
-    ax2.set_ylim(min(np.imag(e)), max(np.imag(e)))
-    
-    return ax1, ax2
 
 class Lorentz:
     """Lorentz Oscillator Class"""
     
-<<<<<<< HEAD
     def __init__(self, A=22, E0=10, G = 0.1):
         self.E = np.linspace(E0-5, E0+20, 100)
         self.A = A #Amplitude [dimentionless]
@@ -84,51 +48,6 @@ class Lorentz:
         l1, = plt.plot(E, np.real(dp))
         l2, = plt.plot(E, np.imag(dp))
         plt.show()
-=======
-    def __init__(self, E=np.linspace(5, 15, 100), A=10, E0=11, G = 1):
-        self.E = E
-        self.A = A
-        self.E0 = E0
-        self.G = G
-        self.param = {'Amp.': self.A,
-                      'E0': self.E0,
-                      'Damping': self.G}
-
-        return None
-
-    def func(self, E):
-
-        A = self.A
-        E0 = self.E0
-        G = self.G
-
-        e1 = []
-        e2 = []
-        
-        for l in E:
-            e1.append(1 + A**2*((E0**2-l**2)/((E0**2-l**2)**2+(G*l)**2)))
-            e2.append((A**2*G*l)/((E0**2-l**2)**2+(G*l)**2))
-        
-        return np.array(e1) +  np.array(e2)*1j
-
-    def plot(self, units='eV'):
-        plot(self, units='eV')
-        
-
-    def table(self):
-        table = []
-        count = 0
-        param = self.param
-        for p in param:
-            table.append([count, p, param[p]])
-            count +=1
->>>>>>> 93145dbd2f30fc0419dea95bf54644b6a201b9d2
-
-        print tabulate(table,
-                       headers = ['#', 'Parameter', 'Value'],
-                       tablefmt = 'orgtbl')
-            
-        
 
 class Material:
     def __init__(self, E=np.linspace(0.5, 6, 1000)):
